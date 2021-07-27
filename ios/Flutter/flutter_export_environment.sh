9aa8a63f11d524bc95c8ae36692beeeb6af8d0b6
Ezequiel Chao <ezesoftchao@gmail.com>
2021-07-27 18:19:16 -0400

    Add files via upload

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/ezequielchao/Documents/SDK/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/ezequielchao/Documents/Projects/soscuba"
+export "FLUTTER_TARGET=/Users/ezequielchao/Documents/Projects/soscuba/lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_DEFINES=flutter.inspector.structuredErrors%3Dtrue"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=/Users/ezequielchao/Documents/Projects/soscuba/.dart_tool/package_config.json"
---
+++ ios/Pods/Target Support Files/psiphon/psiphon-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_psiphon : NSObject
+@end
+@implementation PodsDummy_psiphon
+@end
